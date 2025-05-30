99e33d9cc891f65485478c7ccb3a4def2168f4e0
Jae Min Im <jaeminim@berkeley.edu>
2025-05-30 16:46:38 -0700

    left/right arrow button

--- frontend/src/pages/Calendar/CalendarController.tsx
+++ frontend/src/pages/Calendar/CalendarController.tsx
@@ -4,8 +4,12 @@ import { Button } from "react-bootstrap";
 
 const CalendarController = () => (
   <div className="calendar-controller">
-    <button className="nav-btn">←</button>
-    <button className="nav-btn">→</button>
+    <button className="nav-btn">
+      <img src="/src/assets/left_arrow_circle.png" alt="Left" />
+    </button>
+    <button className="nav-btn">
+      <img src="/src/assets/right_arrow_circle.png" alt="Right" />
+    </button>
     <Button>Today</Button>
     <button className="plus-btn">+</button>
   </div>
